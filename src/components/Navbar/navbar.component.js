6295fdaa42828775681cb4790eb5926fcde5eb08
abhishek-620 <abhishekmangrule620@gmail.com>
2022-01-17 16:44:55 +0530

    nav ml

--- src/components/Navbar/navbar.component.js
+++ src/components/Navbar/navbar.component.js
@@ -1,31 +1,52 @@
 import React from "react";
-import {BiChevronRight} from "react-icons/bi"
+import {BiChevronRight, BiSearch} from "react-icons/bi"
 const NavSm = () => {
   return(
     <>
+    <div className="text-white flex items-center justify-between">
     <div>
-    <div>
-    <h3>It All Starts Here!</h3>
-    <span>
-    Bhubaneswar <BiChevronRight />
+    <h3 className="text-xl font-bold">It All Starts Here!</h3>
+    <span className="text-gray-400 text-xs flex items-center">
+    Bhubaneswar
+    <BiChevronRight />
     </span>
+    </div>
+    <div  className="w-8 h-8">
+    <BiSearch className="w-full h-full" />
     </div>
     </div>
     </>
   )
 };
 
-const NavMd = () => {};
+const NavMd = () => {
+  return(
+    <>
+    <div className="w-full flex gap-3 items-center bg-white px-3 py-2 rounded-md">
+    <BiSearch />
+    <input
+    type="search"
+    className="w-full"
+    placeholder="Search for Movies, Events, Plays, Sports and Activities"
+    />
+    </div>
+    </>
+  );
+};
 const NavLg = () => {};
 
 const Navbar = () => {
   return (
     <>
-    <nav>
+    <nav className="bg-gray-800 px-2 py-4">
     <div className="md:hidden">{/*mobile screen*/}
     <NavSm />
     </div>
-    <div className="hidden md:flex">{/*tablet screen*/}</div>
+    <div className="hidden md:flex">{/*tablet screen*/}
+    <NavMd />
+
+    </div>
+
     <div className="hidden lg:flex">{/*large screen*/}</div>
     </nav>
     </>
